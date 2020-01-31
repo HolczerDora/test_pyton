e1665b1ca81c23a80127179c55124755a65c76d8
HolczerDora <hdwdart@gmail.com>
2020-01-31 13:22:00 +0100

    Update teknos.py
    add new data

--- teknos.py
+++ teknos.py
@@ -5,7 +5,7 @@ import turtle
 ablak = turtle.Screen()
 ablak.setup(width=800,height=600)
 ablak.bgcolor("purple")
-ablak.title("Pingi-Pongi")
+ablak.title("Pong Game")
 ablak.tracer()
 
 # Bal oldali ütő
@@ -17,6 +17,58 @@ balUto.color("white")
 balUto.penup()
 balUto.goto(-350,0)
 
+# Jobb oldali ütő
+jobb = turtle.Turtle()
+jobb.speed(0)
+jobb.shape("square")
+jobb.shapesize(stretch_wid=5,stretch_len=1)
+jobb.color("white")
+jobb.penup()
+jobb.goto(350,0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("black")
+ball.penup()
+ball.goto(0,0)
+#változók
+ball.valtozoX = -1
+ball.valtozoY = -1
+
+def bal_uto_up():
+    y = balUto.ycor()
+    y+=30 # y=y+30
+    balUto.sety(y)
+
+def bal_uto_down():
+    y = balUto.ycor()
+    y-=30 # y=y-30
+    balUto.sety(y)    
+
+def jobb_uto_up():
+    y = jobb.ycor()
+    y+=30 # y=y+30
+    jobb.sety(y) 
+
+def jobb_uto_down():
+    y = jobb.ycor()
+    y-=30 # y=y-30
+    jobb.sety(y)       
+
+ablak.onkey(bal_uto_up,"e")
+ablak.onkey(bal_uto_down,"f")
+ablak.onkey(jobb_uto_up,"p")
+ablak.onkey(jobb_uto_down,"l")
+
+# a képernyő figyelése
+ablak.listen()
+
 while True:
-    ablak.update()
+    # a képernyő frissítése
+    ablak.update() 
+    #a labdát figyeli, az aktuális pozicíójához ad hozzá vagy vesz el
+    ball.setx(ball.xcor() + ball.valtozoX)
+    ball.sety(ball.ycor() - ball.valtozoY)
 

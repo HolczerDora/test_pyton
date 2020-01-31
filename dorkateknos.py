ea5d10c70715747c2da8731bdc2f06ef75199c3f
HolczerDora <hdwdart@gmail.com>
2020-01-31 11:48:17 +0100

    add new data for turtle

---
+++ dorkateknos.py
@@ -0,0 +1,25 @@
+import turtle
+
+# ABLAK
+
+window = turtle.Screen()
+window.setup(width=900,height=900)
+window.bgcolor("purple")
+window.title("Pingi-Pongi")
+window.tracer()
+
+# Bal oldali ütő
+dorka = turtle.Turtle()
+dorka.speed(2)
+dorka.shape("turtle")
+dorka.color("white")
+dorka.forward(200)
+dorka.right(90)
+dorka.forward(200)
+dorka.right(90)
+dorka.forward(200)
+dorka.right(90)
+dorka.forward(200)
+dorka.right(90)
+
+
